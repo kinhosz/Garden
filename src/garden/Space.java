86f584ae9316394f23c69ec6aff0b09016ef0716
kinhosz <jcsc@cin.ufpe.br>
2021-09-12 02:02:07 -0300

    create test

--- src/garden/Space.java
+++ src/garden/Space.java
@@ -7,56 +7,80 @@ import handle.Keyboard;
 import handle.Mouse;
 import bits.Queue;
 import handle.Motion;
+import camera.Camera;
+import event.Event;
+import event.KeyboardEv;
 
 public class Space{
 
-    private BufferedImage image;
-    private JLabel context;
+    private Camera camera;
 
     public Space(int width, int height){
-        this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        this.camera = new Camera(width, height);
     }
 
-    public void setPixel(int x, int y, int r, int g, int b){
-        Color temp = new Color(r,g,b);
-        image.setRGB(x, y, temp.getRGB());
+    public Camera getCamera(){
+        return this.camera;
     }
 
-    public JLabel getContext(){
-        context = new JLabel(new ImageIcon(this.image));
-        return context;
+    public double getXCameraLocation(){
+        return this.camera.getXLocation();
+    }
+
+    public double getYCameraLocation(){
+        return this.camera.getYLocation();
+    }
+
+    public double getZCameraLocation(){
+        return this.camera.getZLocation();
     }
 
     public static void main(String[] args){
-        JFrame frame = new JFrame("teste");
+        JFrame frame = new JFrame("Teste");
         Space space = new Space(300,300);
         Queue buffer = new Queue();
-        Mouse mouse = new Mouse(buffer);
         Keyboard keyboard = new Keyboard(buffer);
+        Mouse mouse = new Mouse(buffer);
         Motion motion = new Motion(buffer);
 
         frame.addKeyListener(keyboard);
         frame.addMouseListener(mouse);
         frame.addMouseMotionListener(motion);
 
+        Camera camera = space.getCamera();
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(space.getContext());
+        frame.getContentPane().add(camera.takePicture());
         frame.pack();
         frame.setVisible(true);
-        for(int i=0;i<255;i++){
-            for(int j=0;j<255;j++){
-                for(int k=0;k<255;k++){
-                    
-                    //frame.remove(space.getContext());
-                    for(int x=0;x<300;x++){
-                        for(int y=0;y<300;y++){
-                            space.setPixel(x, y, i, j, k);
-                        }
-                    }
-                    frame.getContentPane().add(space.getContext());
-                    frame.pack();
-                    frame.setVisible(true);
+
+        while(true){
+
+            if(buffer.size() == 0) continue;
+            Event ev = buffer.front();
+            buffer.pop();
+
+            if(ev instanceof KeyboardEv){
+                KeyboardEv e = (KeyboardEv) ev;
+                if(e.getKeyChar() == 'f' || e.getKeyChar() == 'F'){
+                    break;
                 }
+                else if(e.getKeyChar() == 'w'){
+                    camera.moveFront();
+                }
+                else if(e.getKeyChar() == 'd'){
+                    camera.moveRight();
+                }
+                else if(e.getKeyChar() == 's'){
+                    camera.moveBack();
+                }
+                else if(e.getKeyChar() == 'a'){
+                    camera.moveLeft();
+                }
+                System.out.println("----------------------");
+                System.out.println(space.getXCameraLocation());
+                System.out.println(space.getYCameraLocation());
+                System.out.println(space.getZCameraLocation());
             }
         }
     }
