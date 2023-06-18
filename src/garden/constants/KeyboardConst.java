4122192ca3bef40a39384409536ad3c91d15e334
kinhosz <jcsc@cin.ufpe.br>
2023-06-18 04:54:21 -0300

    refactor(src): move to com.garden

--- src/garden/constants/KeyboardConst.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-public interface KeyboardConst {
-    public static final int KEY_PRESSED = 1;
-    public static final int KEY_TYPED = 2;
-    public static final int KEY_RELEASED = 3;
-}
--- src/garden/constants/SourceConst.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public interface SourceConst {
-    public static final int KEYBOARD = 1;
-    public static final int MOUSE = 2;
-    public static final int MOTION = 3;
-    public static final int TIMER = 4;
-}
